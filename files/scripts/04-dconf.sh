6c46f0f8633dbdb071064f9cbbebc621f31e373e
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2025-04-03 12:05:52 -0500

    try dconf again

--- files/scripts/04-dconf.sh
+++ files/scripts/04-dconf.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-# Load dconf settings for GNOME interface
-dconf load /org/gnome/desktop/interface/ <<EOF
-[org/gnome/desktop/interface]
-icon-theme='Kora'
-cursor-theme='Bibata-Modern-Classic'
-cursor-size=24
-EOF
+echo "Updating dconf database..."
+dconf update
