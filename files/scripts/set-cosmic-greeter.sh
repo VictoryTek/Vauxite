b0a3cd1bc6734294603a4e76d8b37ff7f8106050
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2025-05-24 10:49:37 -0500

    try again

---
+++ files/scripts/set-cosmic-greeter.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+# Set cosmic-greeter as the default display manager
+target=/usr/lib/systemd/system/cosmic-greeter.service
+link=/etc/systemd/system/display-manager.service
+if [ -e "$link" ]; then
+  rm -f "$link"
+fi
+ln -s "$target" "$link"
